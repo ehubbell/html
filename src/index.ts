826f3a8c3740ff3d29579d4081313def4d5253b7
playbooks <eric@erichubbell.com>
2025-03-06 11:56:23 -0700

    Clean

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import Accounting from "accounting";
+import Accounting from 'accounting';
 
 export const isEven = (n: number): boolean => {
-  return n % 2 === 0;
+	return n % 2 === 0;
 };
 
 export const isOdd = (n: number): boolean => {
-  return n % 2 !== 0;
+	return n % 2 !== 0;
 };
 
 export const toCurrency = (value = 0, precision = 2) => {
-  return Accounting.formatMoney(value, "$", precision);
+	return Accounting.formatMoney(value, '$', precision);
 };
 
 export const toNumber = (value = 0, decimal = 0) => {
-  return Accounting.formatNumber(value, decimal);
+	return Accounting.formatNumber(value, decimal);
 };
 
 export const testState = (data?) => {
-  const [value, setValue] = useState(null);
+	const [value, setValue] = useState(null);
 
-  // Hooks
-  useEffect(() => {
-    setValue(data);
-  }, [data]);
+	// Hooks
+	useEffect(() => {
+		setValue(data);
+	}, [data]);
 
-  return value;
+	return value;
 };
