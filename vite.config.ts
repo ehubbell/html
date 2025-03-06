ae036ed5264bfb7937494d49ba92cd45a0de4d09
playbooks <eric@erichubbell.com>
2025-03-06 12:18:27 -0700

    Config updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,29 @@
-import path from "path";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+
+import path from 'path';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "mylib",
-      formats: ["es", "cjs", "umd", "iife"],
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-      plugins: [peerDepsExternal()],
-    },
-  },
+	base: './',
+	plugins: [react()],
+	build: {
+		sourcemap: true,
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'HtmlElements',
+			formats: ['es', 'cjs', 'umd', 'iife'],
+			fileName: format => `index.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+			plugins: [peerDepsExternal()],
+		},
+	},
 });
