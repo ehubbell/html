598e0f4e0b43d7c7c7079acb1dc8a346ed65c985
playbooks <eric@erichubbell.com>
2025-06-12 14:28:30 -0600

    package updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,31 @@
 import { exec } from 'node:child_process';
 import path from 'path';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { defineConfig } from 'vite';
 
-function pushBuild() {
-	exec('dts-bundle-generator --config dts.config.ts', (response, error) => {
-		if (error) console.error(error);
-		exec('npx yalc push', (response, error) => (error ? console.error(error) : null));
-	});
+export function pushBuild() {
+	return {
+		name: 'push-build',
+		closeBundle: async () => {
+			exec('dts-bundle-generator --config dts.config.ts', (response, error) => {
+				if (error) console.error(error);
+				if (response) console.log(response);
+				exec('npx yalc push', (response, error) => (error ? console.error(error) : null));
+			});
+		},
+	};
 }
 
 export default defineConfig({
 	base: './',
 	build: {
-		sourcemap: true,
 		lib: {
 			entry: path.resolve(__dirname, 'src/index.tsx'),
 			name: 'HTML',
-			formats: ['es', 'cjs', 'umd', 'iife'],
+			formats: ['es', 'cjs'],
 			fileName: format => `index.${format}.js`,
 		},
-		rollupOptions: {
-			external: ['react', 'react-dom', 'react/jsx-runtime'],
-			output: {
-				globals: {
-					react: 'React',
-					'react-dom': 'ReactDOM',
-					'react/jsx-runtime': 'react/jsx-runtime',
-				},
-			},
-			plugins: [peerDepsExternal()],
-		},
 	},
-	plugins: [
-		{
-			name: 'push-build',
-			closeBundle: pushBuild,
-		},
-	],
+	plugins: [pushBuild()],
 	resolve: {
 		alias: {
 			src: path.resolve(__dirname, '/src'),
