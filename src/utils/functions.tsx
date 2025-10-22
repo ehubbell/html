8db7107e98e8afa0c0fdf338a7aa65e58ae03037
playbooks <eric@erichubbell.com>
2025-10-22 11:23:49 -0600

    util updates

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,11 +1,20 @@
 import { isArray, isEmpty, isObject } from 'src/utils/helpers';
 import { tailwindKeys } from 'src/utils/keys';
 
-export const filterTwData = (data, attrs = [], include = true) => {
+export const filterTwProps = (data, attrs = [], include = true) => {
 	const formattedData = {};
 	Object.keys(data)
 		.filter(key => (include ? attrs.includes(key) : !attrs.includes(key)))
 		.map(key => (formattedData[key] = data[key]));
+	return formattedData;
+};
+
+export const computeTwProps = props => {
+	const formattedData = {};
+	Object.keys(props)
+		.filter((key, i, s) => s.indexOf(key) === i)
+		.filter(key => !Object.keys(tailwindKeys).includes(key))
+		.map(key => (formattedData[key] = props[key]));
 	return formattedData;
 };
 
@@ -24,15 +33,6 @@ export const computeTwClassNames = props => {
 			classes = classes.concat(data + ' ');
 		});
 	return classes;
-};
-
-export const computeTwData = props => {
-	const formattedData = {};
-	Object.keys(props)
-		.filter((key, i, s) => s.indexOf(key) === i)
-		.filter(key => !Object.keys(tailwindKeys).includes(key))
-		.map(key => (formattedData[key] = props[key]));
-	return formattedData;
 };
 
 // Docs
